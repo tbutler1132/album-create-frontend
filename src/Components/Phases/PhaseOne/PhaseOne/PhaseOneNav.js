eb292855c2e7385ba88bcc035919a6269d98d7af
github username <tbutler1132@gmail.com>
2021-01-05 18:19:59 -0500

    track_nav

--- src/Components/Phases/PhaseOne/PhaseOne/PhaseOneNav.js
+++ src/Components/Phases/PhaseOne/PhaseOne/PhaseOneNav.js
@@ -1,23 +1,48 @@
 import React from 'react'
 import {NavLink} from 'react-browser-router'
+import {Container, Row, Col, Button} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 
 function PhaseOneNav(props){
 
     return(
         <>
-        {props.songObj.ref_imgs.length > 0 ?
-        <NavLink to={`/tracks/${props.songObj.id}/${props.songObj.phase}/poll`}>Create a poll</NavLink>
-        :
-        null
-        }
-        <br></br>
-        <NavLink to={`/tracks/${props.songObj.id}/${props.songObj.phase}/submitform`}>Submit an Image</NavLink>
-        <NavLink to={`/tracks/${props.songObj.id}/${props.songObj.phase}/images`}>
-        <h5>See all Images</h5>
-        </NavLink>
-        <NavLink to={`/tracks/${props.songObj.id}/${props.songObj.phase}/thread`}>
-                    View Discussion
-        </NavLink>
+        <Container>
+            <Row>
+
+            <Col>   
+                {props.songObj.ref_imgs.length > 0 ?
+                <LinkContainer to={`/tracks/${props.songObj.id}/${props.songObj.phase}/poll`}>
+                    <Button variant='outline-success'>Generate a poll</Button>
+                </LinkContainer>
+                :
+                null
+                }
+            </Col> 
+            </Row>
+        <Row>
+
+
+        
+        <Col>
+            <LinkContainer to={`/tracks/${props.songObj.id}/${props.songObj.phase}/submitform`}>
+                <Button variant="outline-light">Submit an Image</Button>
+            </LinkContainer>
+        </Col>
+
+        <Col>
+            <LinkContainer to={`/tracks/${props.songObj.id}/${props.songObj.phase}/images`}>
+                <Button variant="outline-light">See all Images</Button>
+            </LinkContainer>
+        </Col>
+        
+        <Col>
+            <LinkContainer to={`/tracks/${props.songObj.id}/${props.songObj.phase}/thread`}>
+                        <Button variant="outline-light">View Discussion</Button>
+            </LinkContainer>
+        </Col>
+        </Row>
+        </Container>
         </>
     )
 }
